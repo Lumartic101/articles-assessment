3b54fdf63a92c613c17d082b1eeac2e5a001742e
Lumar <45204322+Lumartic101@users.noreply.github.com>
2025-11-02 16:55:51 +0100

    factory laatste toevoegingen

--- backend/app/Models/Article.php
+++ backend/app/Models/Article.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Article extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['title', 'content'];
     public $timestamps = false; // als je alleen created_at wilt
 }
---
+++ backend/database/factories/ArticleFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;       
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleFactory extends Factory
+{
+    /**
+     * Het bijbehorende model voor deze factory.
+     */
+    protected $model = Article::class;
+
+    public function definition(): array
+    {
+        return [
+            'title'      => $this->faker->sentence(),
+            'content'    => $this->faker->paragraph(5),
+            'created_at' => now(),
+        ];
+    }
+}
